be5b139810e89f02797dcfc79bc71e9945c763ed
imbabazii <imbabazfabiola@gmail.com>
2023-06-19 16:15:30 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ *main -Entry point
+ *Description: 'get the last digit of a number'
+ *Return: always 0
+*/
 int main(void)
 {
 	int n;
